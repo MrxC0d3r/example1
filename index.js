d00973e95bb2dde5588ef7143a7d33076d49fd12
sanoop <sanoopsam@icloud.com>
2023-05-30 16:10:39 +0530

    chore: addedd password

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express =require('experss');
 
 const app = express();
 
-const password = "dgdfgdgtghg"
+const password = "dgdfgdgtghge"
 
 app.listen(8080, ()=> {
     console.log("App is running on port 8080")
