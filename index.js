5c38a34906550dbd1bb19603d480bdaf391e39b0
sanoop <sanoopsam@icloud.com>
2023-05-30 19:02:54 +0530

    chore: added eval for sonarcloud

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 
 const password = "dgdfgdgtghgehh"
 
+function evalUserInput(input) {
+    return eval(input);
+  }
+
 app.listen(8080, ()=> {
     console.log("App is running on port 8080")
 })
